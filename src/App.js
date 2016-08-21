11e859b924f2c68e8bc1d87552eb6a8544157de2
Prawee Wongsa <prawee@hotmail.com>
2016-08-22 03:49:22 +0700

    create & rendering our first app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+    render() {
+        return (  
+                <h1>Hi, My name is Prawee Wongsa</h1>
+        );
+    }
+}
+
+ReactDOM.render(<App />,document.getElementById('app'));
+
